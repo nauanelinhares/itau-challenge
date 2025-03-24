3da07c71ed7d6fd895f5579434cb5c39f2673a6e
nauanelinhares <nauane.linhares@gmail.com>
2025-03-23 23:31:11 -0300

    feat: add kotlin project

---
+++ Makefile
@@ -0,0 +1,14 @@
+# Compile the program
+
+start:
+	gradle build && gradle run
+
+# Build the program
+build:
+	gradle build
+
+# Run the tests
+test:
+	gradle test
+
+# Clean the build directory
---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,10 @@
+package challenge
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing { get("/") { call.respondText("Hello World!") } }
+    routing { get("/ping") { call.respondText("pong ;)") } }
+}
